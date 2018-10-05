e29ed2e064e9412ccfe309d4d435627630aef0c6
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2018-10-06 00:08:44 +0900

    Initial Commit

---
+++ app/src/main/java/jp/crowdworks/job_offer_search/JobOfferSearchApplication.kt
@@ -0,0 +1,10 @@
+package jp.crowdworks.job_offer_search
+
+import android.app.Application
+
+class JobOfferSearchApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/jp/crowdworks/job_offer_search/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.crowdworks.job_offer_search.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.crowdworks.job_offer_search.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
